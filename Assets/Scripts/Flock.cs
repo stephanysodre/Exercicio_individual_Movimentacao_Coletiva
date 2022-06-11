0a9a62b14130ad7dfaf24d31ad3290e51931cb0a
stephany g sodre <69463672+stephanysodre@users.noreply.github.com>
2022-06-10 21:21:41 -0300

    fskgl
    sfhsgfr

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -4,63 +4,105 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-    //
+    // declarando variaveis
     public FlockManager myManager;
-    float speed;
-    //
+    public float speed;
+    bool turning = false;
+    // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+        // declarando a velocidade do cardume o min e o max randomicamente
+        speed = Random.Range(myManager.minSpeed,
+        myManager.maxSpeed);
     }
-    //
-    private void Update()
+    void Update()
     {
-        ApplyRules();
+        // delimita a onde os peixes vao rodar atraves do pilar
+        Bounds b = new Bounds(myManager.transform.position, myManager.swinLimits * 2);
+        RaycastHit hit = new RaycastHit();
+
+        // 
+        Vector3 direction = myManager.transform.position - transform.position;
+
+        // se ele saiu da area limite, para o movimento normal e muda sua direçao para dentro da area de volta
+        if (b.Contains(transform.position) == false)
+        {
+            turning = true;
+            direction = myManager.transform.position - transform.position;
+        }
+
+        // desvia do pilar
+        else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+        {
+            turning = true;
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+        }
+        else
+            turning = false;
+
+        // muda a direçao do movimento, para desviar de obstaculos e entrar devolta dentro da area
+        if (turning)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.LookRotation(direction),
+            myManager.rotationSpeed * Time.deltaTime);
+        }
+        
+        // introduz variaçao na velocidade e direao do movimento dos peixes
+        else
+        {
+
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(myManager.minSpeed,
+                myManager.maxSpeed);
+            if (Random.Range(0, 100) < 20)
+                ApplyRules();
+        }
         transform.Translate(0, 0, Time.deltaTime * speed);
     }
-
+    
     void ApplyRules()
     {
+        //declara a orbitaao, com distancia, velocidade, tamanho do grupo, centro e o circulo que irao percorrer
         GameObject[] gos;
         gos = myManager.allFish;
-
-        Vector3 vcenter = Vector3.zero;
+        Vector3 vcentre = Vector3.zero;
         Vector3 vavoid = Vector3.zero;
         float gSpeed = 0.01f;
         float nDistance;
         int groupSize = 0;
 
+        //  agrupa os peixes conforme as distancias dos outros
         foreach (GameObject go in gos)
-        {
-            if (go!= this.gameObject)
+        {          
+            if (go != this.gameObject)
             {
                 nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                
                 if (nDistance <= myManager.neighbourDistance)
                 {
-                    vcenter += go.transform.position;
+                   
+                    vcentre += go.transform.position;
                     groupSize++;
-
-                    if(nDistance < 1.0f)
+                    if (nDistance < 1.0f)
                     {
                         vavoid = vavoid + (this.transform.position - go.transform.position);
-
                     }
-
                     Flock anotherFlock = go.GetComponent<Flock>();
                     gSpeed = gSpeed + anotherFlock.speed;
                 }
             }
         }
-        if (groupSize > 0)
-        {
-            vcenter = vcenter / groupSize;
-            speed = gSpeed / groupSize;
 
-            Vector3 direction = (vcenter + vavoid) - transform.position;
+        // define a posiçao central do grupo
+        if(groupSize > 0)
+        { 
+            
+            vcentre = vcentre/groupSize + (myManager.goalPos - this.transform.position);
+            speed = gSpeed/groupSize;
+            Vector3 direction = (vcentre + vavoid) - transform.position;      
             if (direction != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
-            }
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed* Time.deltaTime);
         }
     }
 }
--- Assets/Scripts/FlockManager.cs
+++ Assets/Scripts/FlockManager.cs
@@ -4,33 +4,54 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
-    // 
+    // declara variaveis
     public GameObject fishPrefab;
     public int numFish = 20;
     public GameObject[] allFish;
-    public Vector3 swinLimits = new Vector3 (5, 5, 5);
-    //
-    [Header("Configuraçoes do Cardume")]
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos;
+
+    // delara valores da velocidade como min, max,  a distancia entre eles e a rotaçao
+    [Header("Configurações do Cardume")]
     [Range(0.0f, 5.0f)]
     public float minSpeed;
     [Range(0.0f, 5.0f)]
     public float maxSpeed;
     [Range(1.0f, 10.0f)]
     public float neighbourDistance;
-    [Range(5.0f, 5.0f)]
+    [Range(0.0f, 5.0f)]
     public float rotationSpeed;
-    //
-    private void Start()
+
+    void Start()
     {
+        // coloca os peixes na cena aleatoriamente
         allFish = new GameObject[numFish];
+
         for (int i = 0; i < numFish; i++)
         {
-            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
-                                                                Random.Range(-swinLimits.y, swinLimits.y),
-                                                                Random.Range(-swinLimits.z, swinLimits.z));
+            //coloca os peixes em posiçao aleatoria detro da posiçao que foi declarada em bound
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x,
+            swinLimits.x),
+            Random.Range(-swinLimits.y,
+            swinLimits.y),
+            Random.Range(-swinLimits.z,
+            swinLimits.z));
+            // istancia os peixes
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             allFish[i].GetComponent<Flock>().myManager = this;
+            goalPos = this.transform.position;
         }
+
     }
-    
+    void Update()
+    {
+        // ponto que fica rodando os peixes
+        goalPos = this.transform.position;
+        // introduz variaao no ponto de orbitaçao
+        if (Random.Range(0, 100) < 10)
+            goalPos = this.transform.position + new Vector3(Random.Range(-swinLimits.x,
+            swinLimits.x),
+            Random.Range(-swinLimits.y, swinLimits.y),
+            Random.Range(-swinLimits.z, swinLimits.z));
+    }
 }
